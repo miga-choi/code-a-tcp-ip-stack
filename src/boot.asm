934704f33e152faf567a5b26f13352320f9c0ad5
miga-choi <miga.choi.1004@gmail.com>
2024-08-25 23:18:39 +0900

    add subroutine "clearscreen"

--- src/boot.asm
+++ src/boot.asm
@@ -2,7 +2,10 @@
         ;   16-bit (real mode)
         ;   32-bit (protected mode)
         ;   64-bit (long mode).
-        bits 16 ; specifies that the instructions and registers used are intended for 16-bit mode
+
+        ; Tells  the assembler that the code that follows is intended to be excuted in 16-bit mode.
+        ; This is common in bootloader or BIOS-level programming, where the CPU stars in real mode (16-bit).
+        bits 16
 
 
         ;; The processor has a DS register for the data segment. 
@@ -47,3 +50,62 @@
         ;   The stack is a special area of memory used for storing temporary data like 
         ;   return addresses, local variables, and other function-related information.
         mov sp, 2000h ; 8k of stack space.
+
+        ;; We're now free to use the standard calling convention in order to safely 
+        ;; pass control over to different functions. We can use "push" in order to 
+        ;; push "call-saved" registers on the stack, pass parameters to callee again 
+        ;; with "push", and then use "call" to save the current program counter to 
+        ;; the stack, and perform an unconditional jump to the given label.
+
+        ;; Now that all that is out of the way, let's figure out a way to clear the screen,
+        ;; move the pointer, and write some text. This is where real mode and BIOS interrupt calls
+        ;; come into play. By storing certain registers with certain parameters and then sending a
+        ;; particular opcode to the BIOS as an interrupt, we can do a bunch of cool stuff.
+        ;; For example, by storing 0x07 in the AH register and sending interrupt code 0x10 to the BIOS,
+        ;; we can scroll the window down by a number of rows. Note the the registers AH and AL refer to
+        ;; the most and least significant bytes of the 16 bit register AX. Thus, we could effectively
+        ;; update both their values at once by simply pushing a 16 bit value to AX, however, we'll opt
+        ;; for the clearer approach of updating each 1-byte subregister at a time.
+
+        ;; If you look at the spec, you'll see that we need to set AH to 0x07, and AL to 0x00. 
+        ;; The value of register BH refers to the BIOS color attribute, which for our purposes will be 
+        ;; black background (0x0) behind light-gray (0x7) text, so we must set BH to 0x07. 
+        ;; Registers CX and DX refer to the subsection of the screen that we want to clear. 
+        ;; The standard number of character rows/cols here is 25/80, so we set CH and CL to 
+        ;; 0x00 to set (0,0) as the top left of the screen to clear, and DH as 0x18 = 24, 
+        ;; DL as 0x4f = 79. Putting this all together in a function, we get the following snippet. 
+
+        ; Calls the "clearscreen" subroutine/function.
+        ; The "call" instruction pushes the return address onto the stack and
+        ; then jumps to the "clearscreen" label to execute the code at that location.
+        call clearscreen
+
+; This is the label for the "clearscreen" subroutine.
+clearscreen:
+        ; Saves the current value of the "bp" (base pointer) register onto the stack.
+        push bp
+        mov bp, sp
+
+        ; Push all general-purpose registers ("ax","cx","dx","bx","sp","bp","si","di") onto the stack.
+        pusha
+
+        mov ah, 07h ; tells BIOS to scroll down window
+        mov al, 00h ; clear entire window
+        mov bh, 07h ; white on black
+        mov cx, 00h ; specifies top left of screen as (0,0)
+        mov dh, 18h ; 18h = 24 rows of chars
+        mov dl, 4fh ; 4fh = 79 cols of chars
+
+        ; This triggers a BIOS interrupt, specifically interrupt "10th", which handles video services.
+        int 10h ; calls video interrupt
+
+        ; Restores all the general-purpose register that where saved with.
+        popa
+
+        mov sp, bp
+
+        ; Restore the "bp" register to its original value. This restores the caller's stack frame.
+        pop bp
+
+        ; Returns from the subroutine by popping the return address off the stack and jumping back to that address.
+        ret
